dc54b60141007293b1dc505bdd2b4ea3e360581a
lagneshthakur <lagneshthakur@gmail.com>
2021-06-15 17:06:08 +0530

    removes extra nested folders

--- multi_tenant/MultiTenantWidgetApi/MultiTenantWidgetApi/Models/Tenant.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MultiTenantWidgetApi.Models
-{
-    public class Tenant
-    {
-        public string TenantId { get; set; }
-        public string DatabaseServerName { get; set; }
-        public string DatabaseName { get; set; }
-    }
-}
--- multi_tenant/MultiTenantWidgetApi/MultiTenantWidgetApi/Models/TenantSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MultiTenantWidgetApi.Models
-{
-    public class TenantSettings
-    {
-        public string TenantId { get; set; }
-        public string DefaultConnectionString { get; set; }
-    }
-}
